f2efb77bce3bc4bbbc6f19041f11593e6b680753
Mariem <mariem.ehab234@gmail.com>
2024-07-21 12:34:09 +0000

    Task 2 correct next page and prev

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -48,11 +48,12 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
         """returns a dictionary"""
+        total_pages = math.ceil(len(self.dataset()) / page_size)
         return {
                 'page_size': page_size,
                 'page': page,
                 'data': self.get_page(page, page_size),
-                'next_page': page - 1 if (page > 1) else None,
-                'prev_page': page + 1,
-                'total_pages': math.ceil(len(self.dataset()) / page_size)
+                'next_page': page + 1 if (page < total_pages) else None,
+                'prev_page': page - 1 if (page > 1) else None,
+                'total_pages': total_pages
                }
