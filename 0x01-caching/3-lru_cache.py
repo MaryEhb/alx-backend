da632db58a5540645614bc9ceea76f0740119636
Mariem <mariem.ehab234@gmail.com>
2024-07-24 14:08:15 +0000

    Task 3 move key to end if its found in cache before changing its value

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -17,9 +17,13 @@ class LRUCache(BaseCaching):
         item value for the key key"""
         if key and item:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                removed = self.cache_data.popitem(last=False)
-                print("DISCARD: {}".format(removed[0]))
+                if key in self.cache_data:
+                    self.cache_data.move_to_end(key)
+                else:
+                    removed = self.cache_data.popitem(last=False)
+                    print("DISCARD: {}".format(removed[0]))
             self.cache_data[key] = item
+
 
     def get(self, key):
         """return the value in self.cache_data linked to key"""
