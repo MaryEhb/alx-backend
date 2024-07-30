887555c0f0847055814f935d197f384fca186737
Mariem <mariem.ehab234@gmail.com>
2024-07-30 18:22:31 +0000

    Task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''3. Basic Babel setup'''
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    '''Config class'''
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    '''get locale of a web page'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    '''home route'''
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
