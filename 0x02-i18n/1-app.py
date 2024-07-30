0fac34542a0150285aaadf5b7847e056e68c0d0d
Mariem <mariem.ehab234@gmail.com>
2024-07-30 17:53:25 +0000

    Task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''0. Basic Flask app'''
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    '''Config class'''
+    LANGUAGES = ["en", "fr"]
+    DEFAULT_LOCALE = "en"
+    DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
+
+@app.route('/')
+def home():
+    '''home route'''
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
