fd8842da63ac3eda5ae7e9ca2875d08396dff96c
Mariem <mariem.ehab234@gmail.com>
2024-08-27 12:03:51 +0000

    Task 6

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+import { createQueue } from 'kue';
+
+const job_data = {phoneNumber: 'string', message: 'string'};
+
+const queue = createQueue({name: 'push_notification_code'});
+const job = queue.create('push_notification_code', job_data);
+
+job.on('enqueue', () => {
+  console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed attempt', () => {
+  console.log('Notification job failed');
+});
+
+job.save();
